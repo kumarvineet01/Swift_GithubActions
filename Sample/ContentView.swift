352c8085091e21059367b49aca6badff25a369b9
kumarvineet01 <kumarvineet01@gmail.com>
2022-09-28 15:09:39 +0530

    First Commit

---
+++ Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample
+//
+//  Created by 90307579 on 4/22/20.
+//  Copyright © 2020 Eden Prairie High School. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
